12d040b57f5695902f4ef7bc2ad4a7debd9f05f5
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-07-26 22:16:26 -0400

    feature: added 'updateThought' and 'deleteThought' to 'thoughtController'

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -39,4 +39,46 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  async updateThought(req, res) {
+    try {
+      const { thoughtId } = req.params.thoughtId;
+      const { thoughtText } = req.body;
+
+      const updatedThought = await Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        { $set: { thoughtText } },
+        { new: true }
+      );
+
+      if (!updatedThought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+
+      res.json(updatedThought);
+    } catch (err) {
+      console.log('Something went wrong when updating that thought');
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteThought(req, res) {
+    try {
+      const deletedThought = await Thought.findOneAndDelete({
+        _id: req.params.thoughtId,
+      });
+
+      if (!deletedThought) {
+        return res
+          .status(404)
+          .json({ message: 'No thought found with that id' });
+      }
+
+      res.json(deletedThought);
+      console.log(`Deleted: ${deletedThought}`);
+    } catch (err) {
+      console.log('Something went wrong when deleting that thought');
+      res.status(500).json(err);
+    }
+  },
 };
